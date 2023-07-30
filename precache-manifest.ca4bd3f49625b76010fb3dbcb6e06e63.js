c93b308cb905b52d1b99d1e3f439bcd79c204d21
mehjabin16 <58268562+mehjabin16@users.noreply.github.com>
2023-07-30 07:39:18 +0600

    Updates

---
+++ precache-manifest.ca4bd3f49625b76010fb3dbcb6e06e63.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39158bae3db52287422701958894f50b",
+    "url": "/PS-Website/index.html"
+  },
+  {
+    "revision": "a9b5a441ba7af0018e22",
+    "url": "/PS-Website/static/css/main.daab4d78.chunk.css"
+  },
+  {
+    "revision": "95ab9993b07335a29768",
+    "url": "/PS-Website/static/js/2.0f40f4d1.chunk.js"
+  },
+  {
+    "revision": "a2195ab928ac996c903f4c7d297e4364",
+    "url": "/PS-Website/static/js/2.0f40f4d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b5a441ba7af0018e22",
+    "url": "/PS-Website/static/js/main.57f70173.chunk.js"
+  },
+  {
+    "revision": "e754df4f7ba6ae8417b5",
+    "url": "/PS-Website/static/js/runtime-main.c6db1cda.js"
+  },
+  {
+    "revision": "52e67a7fa200d86dedcea6e720b20a64",
+    "url": "/PS-Website/static/media/BTCX.52e67a7f.jpg"
+  },
+  {
+    "revision": "1335fb4da12e077dd738dfdf1ad77e47",
+    "url": "/PS-Website/static/media/Farewell17.1335fb4d.jpg"
+  },
+  {
+    "revision": "ab5d4476b06f13f1764eaf80024de78f",
+    "url": "/PS-Website/static/media/Photo1.ab5d4476.jpg"
+  },
+  {
+    "revision": "ec59735f70b3b29f89b50e13cc30e0a4",
+    "url": "/PS-Website/static/media/Photo2.ec59735f.jpg"
+  },
+  {
+    "revision": "9207f0f6cc04023a1fcccacb052ac383",
+    "url": "/PS-Website/static/media/Photo3.9207f0f6.jpg"
+  },
+  {
+    "revision": "cbdfdc05fb0c4277d096c0a67d12e3f0",
+    "url": "/PS-Website/static/media/Photo4.cbdfdc05.jpg"
+  },
+  {
+    "revision": "55e7b30df6b7da11bfb4aaf20294df56",
+    "url": "/PS-Website/static/media/Photo5.55e7b30d.jpg"
+  },
+  {
+    "revision": "44412e66f51bbe8bfc51843dde4a249e",
+    "url": "/PS-Website/static/media/Photo6.44412e66.jpg"
+  },
+  {
+    "revision": "1c452a31a47297a3b62535115798393a",
+    "url": "/PS-Website/static/media/Pronoia.1c452a31.jpg"
+  },
+  {
+    "revision": "091117988348ebb19cf9013d330c3f55",
+    "url": "/PS-Website/static/media/ShootatIUT.09111798.jpg"
+  },
+  {
+    "revision": "29f2542deaaed60d4967ab18db6658e8",
+    "url": "/PS-Website/static/media/Team2.29f2542d.jpg"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/PS-Website/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "96edfe6563ee50e70eeb75c8e2f879c5",
+    "url": "/PS-Website/static/media/img1.96edfe65.jpg"
+  },
+  {
+    "revision": "d5b86f2e915c666fe7b94f614f393a45",
+    "url": "/PS-Website/static/media/img11.d5b86f2e.jpg"
+  },
+  {
+    "revision": "7f2379b8b9d024b46cd7ca9f03c5c811",
+    "url": "/PS-Website/static/media/img15.7f2379b8.jpg"
+  },
+  {
+    "revision": "5160a3f08203ebb1649ac4f7a1e5e3e1",
+    "url": "/PS-Website/static/media/img22.5160a3f0.jpg"
+  },
+  {
+    "revision": "d4f45389a465b406371d4b158bab1b64",
+    "url": "/PS-Website/static/media/img6.d4f45389.jpg"
+  },
+  {
+    "revision": "7003df51dd9d51b961cca1d16b0ae56f",
+    "url": "/PS-Website/static/media/img7.7003df51.jpg"
+  },
+  {
+    "revision": "0881d7384b30d264a401587a302fe496",
+    "url": "/PS-Website/static/media/img8.0881d738.jpg"
+  },
+  {
+    "revision": "e27fdd7518c4c9ea63b0e6dda2857e19",
+    "url": "/PS-Website/static/media/img9.e27fdd75.jpg"
+  },
+  {
+    "revision": "20fdb2419f77d08c1df4032ee0286030",
+    "url": "/PS-Website/static/media/imgx.20fdb241.jpg"
+  },
+  {
+    "revision": "40c0bbf26ddc0ad7cc8d47f0938ba8be",
+    "url": "/PS-Website/static/media/logo.40c0bbf2.svg"
+  },
+  {
+    "revision": "2b0628745a2929ffcab706a7708160e0",
+    "url": "/PS-Website/static/media/team.2b062874.jpg"
+  },
+  {
+    "revision": "413ea5222283148cd39d9b012de5bade",
+    "url": "/PS-Website/static/media/team3.413ea522.jpg"
+  }
+]);
